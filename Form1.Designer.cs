72dc4caacb098b7641ffdcf9d8468a81e900da87
Daniel Guarilha <danielr.guarilha@gmail.com>
2024-08-29 14:58:34 -0300

    Interface que consome API que gera mensagens motivacionais e de humor

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ApiConselho
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrase = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(251, 159);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 2;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Clique em \"Gerar\" para obter uma mensagem descontraída:";
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.AutoSize = true;
+            this.txtFrase.ForeColor = System.Drawing.Color.Red;
+            this.txtFrase.Location = new System.Drawing.Point(122, 102);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(16, 13);
+            this.txtFrase.TabIndex = 4;
+            this.txtFrase.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 276);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtFrase;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using System.Text.Json;
+
+namespace ApiConselho
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        class Slip
+        {
+            public int id { get; set; }
+            public string advice { get; set; }
+        }
+
+        class Retorno
+        {
+            public Slip slip { get; set; }
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://api.adviceslip.com");
+            var request = new RestRequest("advice", Method.Get);
+            var response = client.Execute(request);
+
+            if (response.IsSuccessStatusCode) 
+            {
+                Retorno retorno = JsonSerializer.Deserialize<Retorno>(response.Content);
+                txtFrase.Text = retorno.slip.advice;
+            } else
+            {
+                MessageBox.Show(response.ErrorMessage); 
+            }
+                
+        }
+    }
+}
